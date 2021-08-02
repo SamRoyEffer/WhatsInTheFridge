d9bbdc4783d1c08b3081abdda25f46094603d1ec
Jacob <jacob.fitzgerald@outlook.com>
2021-08-02 19:33:31 -0400

    was looking inside commented code, removed it

--- client/src/components/auto_complete.js
+++ client/src/components/auto_complete.js
@@ -34,53 +34,53 @@ export default function AutoComplete(props) {
   //   }
   // });
 
-  // const options = [
-  //   {
-  //     "id": 1,
-  //     "name": "Loyce Veum",
-  //     "username": "Elisha74",
-  //     "email": "Sibyl23@gmail.com",
-  //     "address": {
-  //       "street": "Marisa Stravenue",
-  //       "suite": 14843,
-  //       "city": "East Chaz",
-  //       "zipcode": "25519",
-  //       "geo": {
-  //         "lat": "-63.0619",
-  //         "lng": "129.1105"
-  //       }
-  //     },
-  //     "phone": "(434) 710-7087 x05623",
-  //     "website": "dock.info",
-  //     "company": {
-  //       "name": "Roberts - Osinski",
-  //       "catchPhrase": "Function-based background pricing structure",
-  //       "bs": "integrated orchestrate e-services"
-  //     }
-  //   },
-  //   {
-  //     "id": 2,
-  //     "name": "Delfina Brown",
-  //     "username": "Gaylord.Schumm88",
-  //     "email": "Sherman76@gmail.com",
-  //     "address": {
-  //       "street": "Stamm Cape",
-  //       "suite": 97728,
-  //       "city": "Camrynton",
-  //       "zipcode": "11743-0194",
-  //       "geo": {
-  //         "lat": "27.6428",
-  //         "lng": "-71.0685"
-  //       }
-  //     },
-  //     "phone": "(139) 659-6166 x8415",
-  //     "website": "arvid.net",
-  //     "company": {
-  //       "name": "Mante Inc",
-  //       "catchPhrase": "Synergistic needs-based superstructure",
-  //       "bs": "front-end reinvent e-services"
-  //     }
-  //   }]
+  const options = [
+    {
+      "id": 1,
+      "name": "Loyce Veum",
+      "username": "Elisha74",
+      "email": "Sibyl23@gmail.com",
+      "address": {
+        "street": "Marisa Stravenue",
+        "suite": 14843,
+        "city": "East Chaz",
+        "zipcode": "25519",
+        "geo": {
+          "lat": "-63.0619",
+          "lng": "129.1105"
+        }
+      },
+      "phone": "(434) 710-7087 x05623",
+      "website": "dock.info",
+      "company": {
+        "name": "Roberts - Osinski",
+        "catchPhrase": "Function-based background pricing structure",
+        "bs": "integrated orchestrate e-services"
+      }
+    },
+    {
+      "id": 2,
+      "name": "Delfina Brown",
+      "username": "Gaylord.Schumm88",
+      "email": "Sherman76@gmail.com",
+      "address": {
+        "street": "Stamm Cape",
+        "suite": 97728,
+        "city": "Camrynton",
+        "zipcode": "11743-0194",
+        "geo": {
+          "lat": "27.6428",
+          "lng": "-71.0685"
+        }
+      },
+      "phone": "(139) 659-6166 x8415",
+      "website": "arvid.net",
+      "company": {
+        "name": "Mante Inc",
+        "catchPhrase": "Synergistic needs-based superstructure",
+        "bs": "front-end reinvent e-services"
+      }
+    }]
 
   // let apiTest = fetch(`https://api.spoonacular.com/food/ingredients/autocomplete?apiKey=9768625974324441a01777879d94c9b2&query=appl&number=5`)
   // .then((res) => res.json())
@@ -89,7 +89,7 @@ export default function AutoComplete(props) {
 
   return (
     <form>
-  <Select options={apiTest} onChange={(values) => this.onChange(values)} searchBy={'name'}
+  <Select options={options} onChange={(values) => this.onChange(values)} searchBy={'name'}
     labelField={"name"}
     valueField={"name"}
   />
