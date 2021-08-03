43bcbd5091a23d13ad79c6a42ad91ba2e0fc2485
Sam Roy-Effer <samroyeffer@gmail.com>
2021-08-03 14:34:51 -0400

    change header to nav

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -3,18 +3,20 @@ import React from "react";
 export default function Header() {
   return (
     <main>
-      {userCookie ? (
-        <section className="header-login">
-          <button>Home</button>
-          <button>Fridge</button>
-          <button>Logout</button>
-        </section>
-      ) : (
-        <section className="header-not-log">
-          <button>Login</button>
-          <button>Home</button>
-        </section>
-      )}
+      <nav>
+        {userCookie ? (
+          <section className="header-login">
+            <button>Home</button>
+            <button>Fridge</button>
+            <button>Logout</button>
+          </section>
+        ) : (
+          <section className="header-not-log">
+            <button>Login</button>
+            <button>Home</button>
+          </section>
+        )}
+      </nav>
     </main>
   );
 }
