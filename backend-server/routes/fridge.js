259e946059385ae2a3fc1abfffcd42e40fe74db0
Sam Roy-Effer <samroyeffer@gmail.com>
2021-08-05 13:45:44 -0400

    add routes for fridge

--- backend-server/routes/fridge.js
+++ backend-server/routes/fridge.js
@@ -2,14 +2,21 @@ const router = require("express").Router();
 
 module.exports = function (db) {
   router.get("/fridge", (req, res) => {
-    db.query(
-      `SELECT * FROM fridge
-      JOIN fridge on fridge.user_id = users.id 
-      WHERE users.id = $1`,
-      [user.id]
-    ).then(({ rows: fridge }) => {
-      res.json();
+    db.query(`SELECT * FROM fridge`).then((res) => {
+      return res.rows[0];
     });
+  });
+
+  router.post("/fridge", (req, res) => {
+    db.query(`INSERT INTO ingredients(api_name) VALUES($1) RETURNING *`, [
+      api_name,
+    ])
+      .then((res) => {
+        return res.row[0];
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
   });
 
   return router;
