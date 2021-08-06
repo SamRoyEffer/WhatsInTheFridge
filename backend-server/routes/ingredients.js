93ba3bac736bcd17fcbb52645c1c76b502af2560
Sam Roy-Effer <samroyeffer@gmail.com>
2021-08-06 15:01:14 -0400

    adding routes

--- backend-server/routes/ingredients.js
+++ backend-server/routes/ingredients.js
@@ -7,12 +7,6 @@ module.exports = function (db) {
     });
   });
 
-  router.get("/ingredients/:ingredientName", (req, res) => {
-    db.query(`SELECT * FROM ingredients WHERE name = $1`, []).then((res) => {
-      return res.rows[0];
-    });
-  });
-
   router.post("/ingredients_add", (req, res) => {
     db.query(`INSERT INTO ingredients(api_name) VALUES($1) RETURNING *`, [
       api_name,
